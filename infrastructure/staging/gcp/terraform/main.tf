f146db8e8faa1ca4eff0bc136bc1de88ea7e8614
takeosuzuki <takeosub1@stko.info>
2019-08-20 14:15:43 +0900

    account.jsonのパス修正

--- infrastructure/staging/gcp/terraform/main.tf
+++ infrastructure/staging/gcp/terraform/main.tf
@@ -4,7 +4,7 @@ variable "GOOGLE_COMPUTE_ZONE" {}
 variable "GOOGLE_CLUSTER_NAME" {}
 
 provider "google" {
-  credentials = "${file("account.json")}"
+  credentials = "${file("/root/account.json")}"
   region      = "${var.GOOGLE_COMPUTE_REGION}"
   project     = "${var.GOOGLE_PROJECT_ID}"
 }
