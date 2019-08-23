75abd66a11029b874d23059c4fbc21ae8100f27e
takeosuzuki <takeosub1@stko.info>
2019-08-23 11:59:59 +0900

    table定義の変更

--- tools/mysql/table.sql
+++ tools/mysql/table.sql
@@ -5,10 +5,10 @@ USE loadtest;
 DROP TABLE IF EXISTS user;
 CREATE TABLE user
 (
-  id            bigint(20) NOT NULL,
-  name          VARCHAR(255) NOT NULL,
-  age           bigint(20) NOT NULL,
+  `user_id`     bigint(20) NOT NULL,
+  `user_name`   VARCHAR(255) NOT NULL,
+  `user_age`    bigint(20) NOT NULL,
   `created_at`  datetime(6) NOT NULL,
   `updated_at`  datetime(6) NOT NULL,
-  PRIMARY KEY  id (id)
+  PRIMARY KEY  `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
