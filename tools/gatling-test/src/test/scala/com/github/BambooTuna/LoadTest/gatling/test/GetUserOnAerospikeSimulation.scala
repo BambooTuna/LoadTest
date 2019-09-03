66200330fb90f01c00212756212e8b7fffbb1283
Suzuki Takeo <takeo@stko.info>
2019-09-03 17:49:45 +0900

    Add gatling test case GetUserOnAerospikeSimulation

---
+++ tools/gatling-test/src/test/scala/com/github/BambooTuna/LoadTest/gatling/test/GetUserOnAerospikeSimulation.scala
@@ -0,0 +1,32 @@
+package com.github.BambooTuna.LoadTest.gatling.test
+
+import com.github.BambooTuna.LoadTest.adaptor.routes.json.GetUserRequestJson
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+import io.circe.syntax._
+import io.circe.generic.auto._
+
+class GetUserOnAerospikeSimulation extends Simulation with SimulationConfig {
+
+  val scn = scenario(getClass.getName)
+    .exec(
+      http(getClass.getName)
+        .get("/aerospike/user/get")
+        .headers(Map("Content-Type" -> "application/json"))
+        .body(
+          StringBody(
+            GetUserRequestJson(1L).asJson.noSpaces
+          )
+        )
+        .check(status.is(204))
+    )
+
+  setUp(
+    scn.inject(
+      rampUsers(gatlingUser.numOfUser).during(gatlingUser.rampDuration)
+      //      rampUsersPerSec(1).to(request).during(set.seconds)
+    )
+  ).protocols(httpConf).maxDuration(gatlingUser.entireDuration)
+
+}
