d3bb5f12b4dbbf0192831ddac9e9c50ade9c46eb
Suzuki Takeo <takeo@stko.info>
2019-09-21 03:01:37 +0900

    handlerの追加

--- interface/src/main/scala/com/github/BambooTuna/LoadTest/adaptor/routes/GetUserInfoRoute.scala
+++ interface/src/main/scala/com/github/BambooTuna/LoadTest/adaptor/routes/GetUserInfoRoute.scala
@@ -28,35 +28,40 @@ case class GetUserInfoRoute(useCase: GetUserInfoUseCase)(implicit materializer: 
   def route: Route = extractActorSystem { implicit system =>
     extractRequestContext { _ =>
       entity(as[GetUserInfoRequestJson]) { json =>
-        val f =
-          useCase
-            .run(
-              GetUserInfoCommandRequest(
-                UserDeviceId(json.device_id)
-              )
-            )
-            .runToFuture
-        onSuccess(f) {
-          case GetUserInfoCommandSucceeded(response) =>
-            val result =
-              GetUserInfoResponseJson(
-                Some(
-                  UserInfoJson(
-                    device_id = response.userId.value,
-                    advertiser_id = response.advertiserId.value,
-                    game_install_count = response.gameInstallCount.value
-                  )
+        jsonParseHandle {
+          val f =
+            useCase
+              .run(
+                GetUserInfoCommandRequest(
+                  UserDeviceId(json.device_id)
                 )
               )
-            val entity = HttpEntity(MediaTypes.`application/json`, result.asJson.noSpaces)
-            complete(StatusCodes.OK, entity)
-          case GetUserInfoCommandFailed(e) =>
-            val result = GetUserInfoResponseJson(error_messages = Seq(e))
-            val entity = HttpEntity(MediaTypes.`application/json`, result.asJson.noSpaces)
-            complete(StatusCodes.BadRequest, entity)
-          case e =>
-            val entity = HttpEntity(MediaTypes.`application/json`, e.toString)
-            complete(StatusCodes.BadRequest, entity)
+              .runToFuture
+          onSuccess(f) {
+            case GetUserInfoCommandSucceeded(response) =>
+              val result =
+                GetUserInfoResponseJson(
+                  Some(
+                    UserInfoJson(
+                      device_id = response.userId.value,
+                      advertiser_id = response.advertiserId.value,
+                      game_install_count = response.gameInstallCount.value
+                    )
+                  )
+                )
+              val entity = HttpEntity(MediaTypes.`application/json`, result.asJson.noSpaces)
+              complete(StatusCodes.OK, entity)
+            case GetUserInfoCommandFailed(e) =>
+              val result = GetUserInfoResponseJson(error_messages = Seq(e))
+              val entity = HttpEntity(MediaTypes.`application/json`, result.asJson.noSpaces)
+              complete(StatusCodes.BadRequest, entity)
+            case e =>
+              val entity = HttpEntity(MediaTypes.`application/json`, e.toString)
+              complete(StatusCodes.BadRequest, entity)
+          }
+        } { _ =>
+          val entity = HttpEntity(MediaTypes.`application/json`, "json parse error!")
+          complete(StatusCodes.MisdirectedRequest, entity)
         }
       }
     }
