1d361e2c01cb041022ee5208e3041a8c85ebb44e
Suzuki Takeo <takeo@stko.info>
2019-09-20 14:20:16 +0900

    fmt code

--- e2e-test/src/it/scala/ServerInterface.scala
+++ e2e-test/src/it/scala/ServerInterface.scala
@@ -3,15 +3,15 @@ import akka.http.scaladsl.model.StatusCode
 case class Response(statusCode: StatusCode, body: String)
 
 case class BidRequestRequest(
-                                  id: String,
-                                  timestamp: Long,
-                                  device_id: String,
-                                  banner_size: Int,
-                                  media_id: Int,
-                                  os_type: Int,
-                                  banner_position: Int,
-                                  floor_price: Double,
-                                )
+                              id: String,
+                              timestamp: Long,
+                              device_id: String,
+                              banner_size: Int,
+                              media_id: Int,
+                              os_type: Int,
+                              banner_position: Int,
+                              floor_price: Double,
+                            )
 case class BidRequestResponse(
                                id: String,
                                bid_price: Double,
@@ -20,9 +20,9 @@ case class BidRequestResponse(
                              )
 
 case class ReduceBudgetFromWinNoticeRequest(
-                                                 id: String,
-                                                 price: Double,
-                                                 is_click: Int //0 or 1
-                                               )
+                                             id: String,
+                                             price: Double,
+                                             is_click: Int //0 or 1
+                                           )
 
 case class ErrorResponseJson(error_code: String, message: String)
